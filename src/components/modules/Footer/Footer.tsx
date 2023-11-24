18b5b91b1b1aa7baf3213a4e7c9c1563df9c8fb6
Medet Ahmetson <ahmetson@zoho.com>
2023-11-25 03:15:54 +0700

    Update the footer

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -2,9 +2,8 @@ import { Box, Link, Text } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const links = {
-  github: 'https://github.com/ethereum-boilerplate/ethereum-boilerplate/',
-  forum: 'https://forum.moralis.io/',
-  moralis: 'https://moralis.io/?utm_source=boilerplatehosted&utm_medium=todo&utm_campaign=ethereum-boilerplate',
+  github: 'https://github.com/ahmetson/user-caring/',
+  presentation: 'https://devpost.com/software/user-caring',
 };
 
 const Footer = () => {
@@ -13,20 +12,14 @@ const Footer = () => {
       <Text>
         ⭐️ Please star this{' '}
         <Link href={links.github} isExternal alignItems={'center'}>
-          boilerplate <ExternalLinkIcon />
+          ahmetson/user-caring <ExternalLinkIcon />
         </Link>
         , every star makes us very happy!
       </Text>
       <Text>
-        🙋 You have questions? Ask them on the{' '}
-        <Link href={links.forum} isExternal alignItems={'center'}>
-          Moralis forum <ExternalLinkIcon />
-        </Link>
-      </Text>
-      <Text>
         📖 Read more about{' '}
-        <Link href={links.moralis} isExternal alignItems={'center'}>
-          Moralis <ExternalLinkIcon />
+        <Link href={links.presentation} isExternal alignItems={'center'}>
+          User Caring presentation <ExternalLinkIcon />
         </Link>
       </Text>
     </Box>
