9fca83d9063a3f199f9855ad5144bd67d11cdb63
Medet Ahmetson <ahmetson@zoho.com>
2023-11-25 04:51:03 +0700

    Add user interface address

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -1,5 +1,13 @@
 import {EvmAddressish} from "@moralisweb3/common-evm-utils";
 
+export const userInterfaceAddress = (chainId: number): string => {
+  if (chainId === 11155111) {
+    return "0x78220f1C11D91f9B5F21536125201bD1aE5CC676";
+  }
+
+  throw `No user interface address at chain id ${chainId}`;
+}
+
 /**
  * Returns list of smartcontracts that cares about their users
  * @returns {Array<string>}
