b8d734f2b83c23741659805688299d02b96de9d3
Medet Ahmetson <milayter@gmail.com>
2023-12-07 11:45:13 +0700

    Fix eslint

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -1,5 +1,3 @@
-import {EvmAddressish} from "@moralisweb3/common-evm-utils";
-
 export const getUserInterfaceAddress = (chainId: number): `0x${string}` => {
   if (chainId === 11155111) {
     return process.env.USER_INTERFACE as `0x${string}` || "0x78220f1C11D91f9B5F21536125201bD1aE5CC676";
